97c386a17593f0a817b48c824c412d19547b4a67
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-11-28 02:10:13 +0900

    処理フロー作成

--- api/src/lib/assets/scraping/scrapingActor.rb
+++ api/src/lib/assets/scraping/scrapingActor.rb
@@ -6,13 +6,40 @@ class Assets::Scraping::ScrapingActor
   end
 
   def exec
-    # TODO: 
+    # TODO: 店舗リストを取得する
+    brothels = generateBrothels()
 
-    actorPage = @mecanizeAgent.get("https://www.cityheaven.net/hokkaido/A0101/A010103/mikado/girlid-21635734/diary/date201911/")
+    # TODO: 店舗に在籍している女優リストを作成する
+    brothelActors = generateBrothelActors()
+
+    # TODO: 女優の写メ日記ページをスクレイピングする
+    actorPage = scrapingActorDiaries()
+    actorPage = @mecanizeAgent.get("https://www.cityheaven.net/hokkaido/A0101/A010103/mikado/girlid-21635734/diary/date201911/")    
+
+    # TODO: 写メ日記に掲載されている画像URLリストを作成する
+    actorImageURLs = generateActorImageURLs()
     elements = actorPage.search("article div a img")
-
     elements.each do |element|
       puts element[:src]
     end
+
+    # TODO: 女優の画像URLリストをDBに登録する
+    upsertActorImageURLs()
   end
+
+  def generateBrothels()
+  end
+
+  def generateBrothelActors()
+  end
+
+  def scrapingActorDiaries()
+  end
+
+  def generateActorImageURLs()
+  end
+
+  def upsertActorImageURLs()
+  end
+
 end
