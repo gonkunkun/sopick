e30989460e4373f220f4ed6cc411516c73c3e802
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-12-03 08:34:05 +0900

    女優のリンクを全て取得する処理を追加

--- api/src/lib/assets/scraping/scrapingActor.rb
+++ api/src/lib/assets/scraping/scrapingActor.rb
@@ -3,23 +3,22 @@ require "mechanize"
 class Assets::Scraping::ScrapingActor
   def initialize
     @mecanizeAgent = Mechanize.new
+    # @mecanizeAgent.ca_file = "../../cacert.pem"
   end
 
   def exec
+    actorId = 21635734
+
     # TODO: 店舗リストを取得する
     brothels = generateBrothels()
 
     # TODO: 店舗に在籍している女優リストを作成する
-    brothelActors = generateBrothelActors()
-
-    # TODO: 女優の写メ日記ページをスクレイピングする
-    actorPage = scrapingActorDiaries()
-    actorPage = @mecanizeAgent.get("https://www.cityheaven.net/hokkaido/A0101/A010103/mikado/girlid-21635734/diary/date201911/")    
+    brothelActors = generateBrothelActors("https://www.cityheaven.net/hokkaido/A0101/A010103/mikado/girllist/")
+    return
 
     # TODO: 写メ日記に掲載されている画像URLリストを作成する
-    actorImageURLs = generateActorImageURLs()
-    elements = actorPage.search("article div a img")
-    elements.each do |element|
+    actorImageURLs = generateActorImageURLs("https://www.cityheaven.net/hokkaido/A0101/A010103/mikado/girlid-21635734/diary/")
+    actorImageURLs.each do |element|
       puts element[:src]
     end
 
@@ -30,14 +29,64 @@ class Assets::Scraping::ScrapingActor
   def generateBrothels()
   end
 
-  def generateBrothelActors()
+
+  # 女優の写メ日記配下に保存されている画像のURLのリストを返却する関数
+  # [params]
+  # url       : お店のトップページ
+  def generateBrothelActors(url)
+    actorsPage = @mecanizeAgent.get(url)
+    pagesLinks = actorsPage.search("div.contensboxin ul.paging center a")
+
+    # ページングがコンテンツ上部と下部に存在するため、割る2している
+    pagesCount = pagesLinks.length / 2
+    # お店のページング数分、処理を回す(「次の女の子へ」が表示されなくなるまでループ)
+    1.upto pagesCount do |i|
+      puts i
+      # 女優の数分、処理を回す
+      actorsURLs = actorsPage.search("ul.girllist li div.girllistimg a")
+      actorsURLs.each do |actorsURL|
+        puts actorsURL[:href]
+      end
+    end
+    # loop do
+    #   puts "test"
+    #   break if !pagesCount[0].text == "次の女の子へ"
+    # end
+    # puts pagesCount[0].text
+    # next if !pagesCount[0][:href].text == "次の女の子へ"
+    # exit
+
   end
 
-  def scrapingActorDiaries()
+
+  # 女優の写メ日記配下に保存されている画像のURLのリストを返却する関数
+  # [params]
+  # url       : 写メ日記のトップページ
+  # [return]
+  # imgURLsAll: 写メ日記の画像URLのリスト（Nokogiri::XML::Element） 
+  def generateActorImageURLs(url)
+    actorPage = @mecanizeAgent.get(url)
+    # 女優の写メリストを格納する変数
+    imgURLsAll = []
+
+    # 日記が投稿されている月数分、処理を回す
+    diaryURLs = actorPage.search("div#diary_archives ul li a")
+    diaryURLs.each do |elmURL|
+      # href要素を持たないelementを除外
+      next if !elmURL[:href]
+
+      # 各月の日記ページへのリンクをクリックし、ページ要素を取得
+      diaryPage = actorPage.link_with(href: elmURL[:href]).click
+
+      # 1月分の日記に登録されている画像のURL一覧を取得する
+      imgURLsOneMonth = diaryPage.search("article.diary_item div.diary_photoframe a img")
+      imgURLsOneMonth.each do |imgURL|
+        imgURLsAll.push(imgURL)
+      end
+    end
+    return imgURLsAll
   end
 
-  def generateActorImageURLs()
-  end
 
   def upsertActorImageURLs()
   end
