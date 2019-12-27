ed1a53aa735bb3d6165ded7f5eee567a19aafff3
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-12-28 00:53:06 +0900

    atom Images.vue修正

--- frontend/src/components/atoms/images/Images.stories.js
+++ frontend/src/components/atoms/images/Images.stories.js
@@ -6,28 +6,48 @@ import { text, number, array } from "@storybook/addon-knobs"
 storiesOf("Images", module).add(
   "Image",
   () => {
-    const actorImage = text("actorImage", "https://www.pakutaso.com/shared/img/thumb/KAZ76_nemof01_TP_V.jpg")
-    const name   = text("name", "Test")
-    const age    = number("age", 25)
-    const tall   = number("tall", 160)
-    const bust   = number("bust", 80)
-    const cup    = text("cup", "C")
-    const waist  = number("waist", 60)
-    const hip    = number("hip", 60)
-    const height = number("height", 300)
     return {
+      props: {
+        actorImage: {
+          default: text("actorImage", "https://www.pakutaso.com/shared/img/thumb/KAZ76_nemof01_TP_V.jpg")
+        },
+        name: {
+          default: text("name", "Test")
+        },
+        age: {
+          default: number("age", 25)
+        },
+        tall: {
+          default: number("tall", 160)
+        },
+        bust: {
+          default: number("bust", 80)
+        },
+        cup: {
+          default: text("cup", "C")
+        },
+        waist: {
+          default: number("waist", 60)
+        },
+        hip: {
+          default: number("hip", 60)
+        },
+        height: {
+          default: number("height", 300)
+        }
+      },
       components: { AtomImage },
       template: `
         <atom-image
-          actor-image="${actorImage}"
-          name="${name}"
-          age="${age}"
-          tall="${tall}"
-          bust="${bust}"
-          cup="${cup}"
-          waist="${waist}"
-          hip="${hip}"
-          height="${height}"
+          :actor-image="actorImage"
+          :name="name"
+          :age="age"
+          :tall="tall"
+          :bust="bust"
+          :cup="cup"
+          :waist="waist"
+          :hip="hip"
+          :height="height"
         />
       `
     }
@@ -94,7 +114,7 @@ storiesOf("Images", module).add(
   },
   {
     info: {
-      summary: "Vuetifyで用意されているアイコン"
+      summary: "画像リストを自動的に切り替える"
     }
   }
 )
