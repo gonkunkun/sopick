4ac83c23e0eee24f5c9460e6505c926c00fe73ad
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-12-29 02:35:39 +0900

    APIが返却するJSONデータのフォーマットを規定

--- api/src/app/controllers/actors_controller.rb
+++ api/src/app/controllers/actors_controller.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
 class ActorsController < ApplicationController
-  PER = 24
-
   def index
-    # TODO: ページング処理追加
-    @actors = Actor.includes(:actor_images).where(is_delete: 0, is_exist_diary: 1).order(id: :desc).page(params[:page] ||= 1)
+    actors = Actor
+              .includes(:actor_images).includes(:brothel)
+              .where(is_delete: 0, is_exist_diary: 1)
+              .order(created_at: :asc)
+              .page(params[:page] ||= 1)
     
+    # join先のテーブルのattributeを戻り値に付与する
     options = {}
     options[:include] = [:actor_images, :brothel]
-    json_string = ActorSerializer.new(@actors, options).serialized_json
 
-    render json: json_string
+    render_json(ActorSerializer, actors, options)
   end
 
   def show
-    @actor = Actor.find(params[:id])
-    render json: @actor
+    actor = Actor.find(params[:id])
+    render json: actor
   end
 end
