7dbb3e41e8727674b667ed57c2a2c0e7dd66a6ac
gonkunkun <yuta.sasaki.ipu@gmail.com>
2019-12-30 20:22:40 +0900

    apiルーティングの記載方法を変更

--- frontend/src/pages/actors/index.vue
+++ frontend/src/pages/actors/index.vue
@@ -8,8 +8,9 @@
 
 <script>
 import ActorsList from "@/components/organisms/ActorsList"
-import { RepositoryFactory } from "@/repositories/RepositoryFactory"
-const ActorsRepository = RepositoryFactory.get("actors")
+// import { RepositoryFactory } from "@/repositories/RepositoryFactory"
+// const ActorsRepository = RepositoryFactory.get("actors")
+import JsonApi from "devour-client"
 
 export default {
   components: {
@@ -23,10 +24,48 @@ export default {
   },
   methods: {
     async fetch() {
-      const response = await ActorsRepository.getActors()
-      this.actors = response.data
+      // const response = await ActorsRepository.getActors()
+      // this.actors = response.data
       console.log("actors/index.vue")
       console.log(this.actors)
+
+      const jsonApi = new JsonApi({ apiUrl: "http://localhost:3031" })
+      jsonApi.define("actor", {
+        name: "",
+        duration: "",
+        brothel: {
+          jsonApi: "hasOne",
+          type: "brothel"
+        }
+      })
+      // jsonApi.define("actor", {
+      //   name: "",
+      //   movieActors: {
+      //     jsonApi: "hasMany",
+      //     type: "movieActors"
+      //   },
+      //   movies: {
+      //     jsonApi: "hasMany",
+      //     type: "movies"
+      //   }
+      // })
+      // jsonApi.define("genre", {
+      //   name: "",
+      //   movies: {
+      //     jsonApi: "hasMany",
+      //     type: "genres"
+      //   }
+      // })
+      // jsonApi.define("director", {
+      //   name: "",
+      //   movies: {
+      //     jsonApi: "hasMany",
+      //     type: "directors"
+      //   }
+      // })
+      let { data, errors, meta, links } = jsonApi.findAll("actors", "index")
+      console.log(data)
+      return { data, errors, meta, links }
     }
   }
 }
