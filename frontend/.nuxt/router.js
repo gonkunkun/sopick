981ce0ebfc4fa95f90669a2a7f20122c65674bda
gonkunkun <yuta.sasaki.ipu@gmail.com>
2020-01-03 20:44:38 +0900

    Frontの環境をDocker化

--- frontend/.nuxt/router.js
+++ frontend/.nuxt/router.js
@@ -3,11 +3,11 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _2826b7a7 = () => interopDefault(import('../src/pages/actors/index.vue' /* webpackChunkName: "pages/actors/index" */))
-const _8ef4cf00 = () => interopDefault(import('../src/pages/posts/index.vue' /* webpackChunkName: "pages/posts/index" */))
-const _f554e064 = () => interopDefault(import('../src/pages/posts/new.vue' /* webpackChunkName: "pages/posts/new" */))
-const _719fbae8 = () => interopDefault(import('../src/pages/posts/_id.vue' /* webpackChunkName: "pages/posts/_id" */))
-const _78e9a93c = () => interopDefault(import('../src/pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _3b048c16 = () => interopDefault(import('../src/pages/actors/index.vue' /* webpackChunkName: "pages/actors/index" */))
+const _0373fa31 = () => interopDefault(import('../src/pages/posts/index.vue' /* webpackChunkName: "pages/posts/index" */))
+const _0f22a9bf = () => interopDefault(import('../src/pages/posts/new.vue' /* webpackChunkName: "pages/posts/new" */))
+const _0926564e = () => interopDefault(import('../src/pages/posts/_id.vue' /* webpackChunkName: "pages/posts/_id" */))
+const _27eb63ad = () => interopDefault(import('../src/pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -27,23 +27,23 @@ export const routerOptions = {
 
   routes: [{
     path: "/actors",
-    component: _2826b7a7,
+    component: _3b048c16,
     name: "actors"
   }, {
     path: "/posts",
-    component: _8ef4cf00,
+    component: _0373fa31,
     name: "posts"
   }, {
     path: "/posts/new",
-    component: _f554e064,
+    component: _0f22a9bf,
     name: "posts-new"
   }, {
     path: "/posts/:id",
-    component: _719fbae8,
+    component: _0926564e,
     name: "posts-id"
   }, {
     path: "/",
-    component: _78e9a93c,
+    component: _27eb63ad,
     name: "index"
   }],
 
