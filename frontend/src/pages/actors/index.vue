7c728627305948847d16fba330489d49fff5ae87
gonkunkun <yuta.sasaki.ipu@gmail.com>
2020-01-03 15:39:23 +0900

    #35 API通信処理を共通化

--- frontend/src/pages/actors/index.vue
+++ frontend/src/pages/actors/index.vue
@@ -8,9 +8,8 @@
 
 <script>
 import ActorsList from "@/components/organisms/ActorsList"
-// import { RepositoryFactory } from "@/repositories/RepositoryFactory"
-// const ActorsRepository = RepositoryFactory.get("actors")
-import JsonApi from "devour-client"
+import { RepositoryFactory } from "@/repositories/RepositoryFactory"
+const ActorsRepository = RepositoryFactory.get("actors")
 
 export default {
   components: {
@@ -26,52 +25,7 @@ export default {
   },
   methods: {
     async fetch() {
-      // const response = await ActorsRepository.getActors()
-      // this.actors = response.data
-      console.log("actors/index.vue")
-      console.log(this.actors)
-
-      const jsonApi = new JsonApi({ apiUrl: "http://localhost:3031" })
-      jsonApi.define("actor", {
-        id: "",
-        brothel_id: "",
-        girl_id: "",
-        name: "",
-        age: "",
-        tall: "",
-        bust: "",
-        cup: "",
-        waist: "",
-        hip: "",
-        actor_page_url: "",
-        updated_at: "",
-        brothel: {
-          jsonApi: "hasOne",
-          type: "brothel"
-        },
-        actor_images: {
-          jsonApi: "hasMany",
-          type: "actor_image"
-        }
-      })
-      jsonApi.define("brothel", {
-        brothel_name: "",
-        brothel_name_en: "",
-        brothel_url: "",
-        prefecture: "",
-        area: "",
-        area_en: "",
-        area_id: "",
-        area_detail_id: "",
-        brothel_type: ""
-      })
-      jsonApi.define("actor_image", {
-        image_path: "",
-        updated_at: ""
-      })
-
-      let { data, meta, links } = await jsonApi.findAll("actors")
-      console.log(meta)
+      let { data, meta, links } = await ActorsRepository.getActors()
       this.actors = data
       this.pagination = meta.pagination
       this.links = links
--- frontend/src/repositories/actorsRepository.js
+++ frontend/src/repositories/actorsRepository.js
@@ -1,13 +1,52 @@
 import Repository from "./Repository"
 
-const resource = "/actors"
-export default {
-  // get() {
-  //   return Repository.get(`${resource}`)
-  // },
+const resource = "actors"
+// viewの定義
+const jsonApi = Repository.jsonApi
+jsonApi.define("actor", {
+  id: "",
+  brothel_id: "",
+  girl_id: "",
+  name: "",
+  age: "",
+  tall: "",
+  bust: "",
+  cup: "",
+  waist: "",
+  hip: "",
+  actor_page_url: "",
+  updated_at: "",
+  brothel: {
+    jsonApi: "hasOne",
+    type: "brothel"
+  },
+  actor_images: {
+    jsonApi: "hasMany",
+    type: "actor_image"
+  }
+})
+jsonApi.define("brothel", {
+  brothel_name: "",
+  brothel_name_en: "",
+  brothel_url: "",
+  prefecture: "",
+  area: "",
+  area_en: "",
+  area_id: "",
+  area_detail_id: "",
+  brothel_type: ""
+})
+jsonApi.define("actor_image", {
+  image_path: "",
+  updated_at: ""
+})
 
-  getActors() {
-    return Repository.get(`${resource}/index`)
+export default {
+  getActors(pageNumber = 1) {
+    // return Repository.get(`${resource}`)
+    return jsonApi.findAll(resource, {
+      page: pageNumber
+    })
   }
 
   // createPost(payload) {
