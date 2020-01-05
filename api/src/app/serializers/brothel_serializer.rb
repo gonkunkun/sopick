1319dec94f12c2c937b73bf5cd814ff0840b1b04
gonkunkun <yuta.sasaki.ipu@gmail.com>
2020-01-05 20:04:18 +0900

    コントローラクラス作成

--- api/src/app/serializers/brothel_serializer.rb
+++ api/src/app/serializers/brothel_serializer.rb
@@ -6,11 +6,13 @@ class BrothelSerializer
              :brothel_name,
              :brothel_name_en,
              :brothel_url,
+             :prefecture_id,
              :prefecture,
              :area,
              :area_en,
              :area_id,
              :area_detail_id,
+             :brothel_type_id,
              :brothel_type
   has_many   :actors
 end
